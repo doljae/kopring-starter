99fe0dd50e96a7041d3adbea333b290fd87779d5
Seokjae Lee <seok9211@naver.com>
2023-06-05 15:03:52 +0900

    build: add refreshVersions plugin

---
+++ buildSrc/settings.gradle.kts
@@ -0,0 +1,12 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+    plugins {
+        id("de.fayard.refreshVersions") version "0.51.0"
+    }
+}
+
+plugins {
+    id("de.fayard.refreshVersions")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,6 @@
+plugins {
+    // See https://jmfayard.github.io/refreshVersions
+    id("de.fayard.refreshVersions") version "0.51.0"
+}
+
 rootProject.name = "kopring"
