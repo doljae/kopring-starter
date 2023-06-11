4ee7564c168b1a2e3c8929da398df00741378867
Seokjae Lee <seok9211@naver.com>
2023-06-11 13:18:37 +0900

    build: apply refreshVersions, add a class to manage project-wide dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.1.0"
-    id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.8.21"
-    kotlin("plugin.spring") version "1.8.21"
-    kotlin("plugin.jpa") version "1.8.21"
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    kotlin("plugin.jpa")
 }
 
 group = "com.doljae"
@@ -23,18 +23,23 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("com.mysql:mysql-connector-j")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.boot:spring-boot-testcontainers")
-    testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.testcontainers:mysql")
+    implementation(Dependencies.kotlinReflect)
+    implementation(Dependencies.jacksonKotlin)
+
+    implementation(Dependencies.springBootStarterWeb)
+    implementation(Dependencies.springBootStarterJpa)
+    implementation(Dependencies.springBootStarterActuator)
+    implementation(Dependencies.springBootStarterTest)
+
+    implementation(Dependencies.springBootConfigurationProcessor)
+    implementation(Dependencies.springBootTestContainers)
+    implementation(Dependencies.springBootDevTools)
+
+    implementation(Dependencies.testContainersJunitJupiter)
+    implementation(Dependencies.testContainersMySql)
+
+    implementation(Dependencies.mySqlConnector)
+
 }
 
 tasks.withType<KotlinCompile> {
---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,18 @@
+object Dependencies {
+    const val kotlinReflect = "org.jetbrains.kotlin:kotlin-reflect"
+    const val jacksonKotlin = "com.fasterxml.jackson.module:jackson-module-kotlin"
+
+    const val springBootStarterWeb = "org.springframework.boot:spring-boot-starter-web"
+    const val springBootStarterJpa="org.springframework.boot:spring-boot-starter-data-jpa"
+    const val springBootStarterActuator= "org.springframework.boot:spring-boot-starter-actuator"
+    const val springBootStarterTest= "org.springframework.boot:spring-boot-starter-test"
+
+    const val springBootConfigurationProcessor = "org.springframework.boot:spring-boot-configuration-processor"
+    const val springBootTestContainers="org.springframework.boot:spring-boot-testcontainers"
+    const val springBootDevTools="org.springframework.boot:spring-boot-devtools"
+
+    const val testContainersJunitJupiter="org.testcontainers:junit-jupiter"
+    const val testContainersMySql="org.testcontainers:mysql"
+
+    const val mySqlConnector="com.mysql:mysql-connector-j"
+}
