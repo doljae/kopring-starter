5d2781607f69a58607f3eb704b350e70a87fd57d
Seokjae Lee <seok9211@naver.com>
2023-07-25 22:31:49 +0900

    chore: specify type explicity

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,25 +1,25 @@
 object Dependencies {
-    const val kotlinReflect = "org.jetbrains.kotlin:kotlin-reflect"
-    const val jacksonKotlin = "com.fasterxml.jackson.module:jackson-module-kotlin"
+    const val kotlinReflect: String = "org.jetbrains.kotlin:kotlin-reflect"
+    const val jacksonKotlin: String = "com.fasterxml.jackson.module:jackson-module-kotlin"
 
-    const val springBootStarterWeb = "org.springframework.boot:spring-boot-starter-web"
-    const val springBootStarterJpa="org.springframework.boot:spring-boot-starter-data-jpa"
-    const val springBootStarterActuator= "org.springframework.boot:spring-boot-starter-actuator"
-    const val springBootStarterTest= "org.springframework.boot:spring-boot-starter-test"
+    const val springBootStarterWeb: String = "org.springframework.boot:spring-boot-starter-web"
+    const val springBootStarterJpa: String = "org.springframework.boot:spring-boot-starter-data-jpa"
+    const val springBootStarterActuator: String = "org.springframework.boot:spring-boot-starter-actuator"
+    const val springBootStarterTest: String = "org.springframework.boot:spring-boot-starter-test"
 
-    const val springBootConfigurationProcessor = "org.springframework.boot:spring-boot-configuration-processor"
-    const val springBootTestContainers="org.springframework.boot:spring-boot-testcontainers"
-    const val springBootDevTools="org.springframework.boot:spring-boot-devtools"
+    const val springBootConfigurationProcessor: String = "org.springframework.boot:spring-boot-configuration-processor"
+    const val springBootTestContainers: String = "org.springframework.boot:spring-boot-testcontainers"
+    const val springBootDevTools: String = "org.springframework.boot:spring-boot-devtools"
 
-    const val testContainersJunitJupiter="org.testcontainers:junit-jupiter"
-    const val testContainersMySql="org.testcontainers:mysql"
+    const val testContainersJunitJupiter: String = "org.testcontainers:junit-jupiter"
+    const val testContainersMySql: String = "org.testcontainers:mysql"
 
-    const val mySqlConnector="com.mysql:mysql-connector-j"
+    const val mySqlConnector: String = "com.mysql:mysql-connector-j"
 
-    const val logging="io.github.oshai:kotlin-logging-jvm:_"
+    const val logging: String = "io.github.oshai:kotlin-logging-jvm:_"
 
-    const val kotestCore = "io.kotest:kotest-runner-junit5-jvm:_"
-    const val kotestAssertions = "io.kotest:kotest-assertions-core-jvm:_"
-    const val mockk = "io.mockk:mockk:_"
-    const val springMockk = "com.ninja-squad:springmockk:_"
+    const val kotestCore: String = "io.kotest:kotest-runner-junit5-jvm:_"
+    const val kotestAssertions: String = "io.kotest:kotest-assertions-core-jvm:_"
+    const val mockk: String = "io.mockk:mockk:_"
+    const val springMockk: String = "com.ninja-squad:springmockk:_"
 }
