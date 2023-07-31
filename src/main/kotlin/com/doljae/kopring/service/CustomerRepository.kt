846253cc4eb46a17a7b885894cc472b3a99dd3cb
Seokjae Lee <seok9211@naver.com>
2023-07-31 09:38:22 +0900

    chore: for sample codes

--- src/main/kotlin/com/doljae/kopring/service/CustomerRepository.kt
+++ src/main/kotlin/com/doljae/kopring/service/CustomerRepository.kt
@@ -1,6 +1,11 @@
 package com.doljae.kopring.service
 
 import com.doljae.kopring.domain.Customer
+import com.doljae.kopring.domain.Gender
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
 
-interface CustomerRepository : JpaRepository<Customer, Long>
+interface CustomerRepository : JpaRepository<Customer, Long> {
+    fun findByGender(gender: Gender): Customer?
+    fun findByFirstName(firstName: String): Optional<Customer>
+}
