2d59987216e10af60b87e9a2a01a9bb61348d994
Seokjae Lee <seok9211@naver.com>
2023-08-08 16:32:04 +0900

    feat: add multiple styles to code that do the same thing

--- src/main/kotlin/com/doljae/kopring/api/TestController.kt
+++ src/main/kotlin/com/doljae/kopring/api/TestController.kt
@@ -1,5 +1,6 @@
 package com.doljae.kopring.api
 
+import com.doljae.kopring.service.TestService
 import com.doljae.kopring.util.InfconProperties
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -7,13 +8,22 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class TestController(
+    private val testService: TestService,
     private val properties: InfconProperties,
 ) {
 
-    @GetMapping("/test")
-    fun test(): ResponseEntity<String> {
-        return ResponseEntity.ok("Hello Kopring!")
+    @GetMapping("/test/v1")
+    fun testApiV1(): ResponseEntity<String> {
+        return ResponseEntity.ok(testService.greeting())
     }
+
+    @GetMapping("/test/v2")
+    fun testApiV2(): ResponseEntity<String> = with(testService.greeting()) {
+        ResponseEntity.ok(this)
+    }
+
+    @GetMapping("/test/v3")
+    fun testApiV3(): ResponseEntity<String> = ResponseEntity.ok(testService.greeting())
 
     @GetMapping("/test/infcon")
     fun testInfcon(): ResponseEntity<InfconProperties> {
