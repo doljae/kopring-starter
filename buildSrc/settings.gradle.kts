68eeab0944a4a5680280a22e13bfbbc290bf797e
Seokjae Lee <seok9211@naver.com>
2023-08-31 22:13:24 +0900

    build: upgrade dependencies including refreshVersions 0.60.2

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -3,7 +3,7 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        id("de.fayard.refreshVersions") version "0.60.1"
+        id("de.fayard.refreshVersions") version "0.60.2"
     }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // See https://jmfayard.github.io/refreshVersions
-    id("de.fayard.refreshVersions") version "0.60.1"
+    id("de.fayard.refreshVersions") version "0.60.2"
 }
 
 rootProject.name = "kopring"
